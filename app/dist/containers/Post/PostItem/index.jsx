081b0595bc713fdfbc60111d62df95db1570c764
ytk <yutingkuang@gmail.com>
2017-11-15 11:38:21 +0800

    作業二的test case (view)

--- app/dist/containers/Post/PostItem/index.jsx
+++ app/dist/containers/Post/PostItem/index.jsx
@@ -6,11 +6,11 @@ export const PostComp: any = (props: any) => {
   const { post: { id, title, userId, body }, index } = props;
   return (
     <div styleName="panel panel-default">
-      <div styleName="panel-body">
+      <div styleName="panel-body" className="panel-body">
         #{index} {title}
         <span styleName="pull-right">By {userId}</span>
       </div>
-      <div styleName="panel-footer">
+      <div styleName="panel-footer" className="panel-footer">
         {body}
         <span styleName="pull-right">
           <a href={`/post/${id}/comments`}>...detail</a>
